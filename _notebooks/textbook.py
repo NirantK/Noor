4fb1bda79aaca0673e5e62061c8f96826516ed03
meghanabhange <meghanabhange13@gmail.com>
2021-01-06 20:12:58 +0530

    Being mean isn't the best way to motivate just saying.

--- _notebooks/textbook.py
+++ _notebooks/textbook.py
@@ -2,11 +2,13 @@ from io import StringIO
 from pathlib import Path
 from typing import List, Union
 
+import pysbd
 import requests
+from pdf_parsing import pdf_to_text
 from pydantic import BaseModel
 from tqdm import tqdm
 
-from pdf_parsing import pdf_to_text
+seg = pysbd.Segmenter(language="en", clean=True)
 
 
 class Chapter(BaseModel):
@@ -55,7 +57,9 @@ class Book(BaseModel):
         try:
             assert self.zip_file_path != ""
         except AssertionError as e:
-            raise AssertionError(f"Please download the file or set the zip_file_path variable")
+            raise AssertionError(
+                f"Please download the file or set the zip_file_path variable"
+            )
         import zipfile
 
         extract_to = Path(extract_to)
@@ -82,7 +86,9 @@ class Book(BaseModel):
             for folder in self.extract_to_path.ls():
                 pdf_files.extend(folder.pdfls())
             pdf_files.sort()
-            pdf_files = [file for file in pdf_files if file.stem[-2:].isdigit()]  # keep the chapter files, nothing else
+            pdf_files = [
+                file for file in pdf_files if file.stem[-2:].isdigit()
+            ]  # keep the chapter files, nothing else
             return pdf_files
 
         pdf_files = get_chapter_pdf_for_book(self)
@@ -102,3 +108,29 @@ class Book(BaseModel):
                 number=int(file.stem[-2:]),
             )
             self.chapters.append(chp)
+
+    def clean_raw_text(self, disable_pysbd=False):
+        """pySBD (Python Sentence Boundary Disambiguation)
+        is a rule-based sentence boundary detection that
+        works out-of-the-box.
+
+        Didn't use the spacy-pipe version of this because it
+        is conflicting with the neuralcoref.
+
+        Each paragraph is separated by \n\n so using that
+        to remove separate it in paragraphs and then removing \n
+        within the paragraphs.
+        Also using pySBD to then identify sentences in each
+        paragraph. I don't know if we need this so added a disable
+        option for this. The difference you can see after disabling
+        it is you will be able to see the whole paragraph as one
+        blob.
+        """
+        for chapter in tqdm(self.chapters):
+            clean = []
+            for text in chapter.raw_text.split("\n\n"):
+                clean.append(text.replace("\n", " "))
+            clean_text = "\n".join(clean)
+            if not disable_pysbd:
+                clean_text = "\n".join(seg.segment(clean_text))
+            chapter.clean_text = clean_text
